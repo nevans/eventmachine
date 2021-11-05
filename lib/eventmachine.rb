544d0324a5baef5806ea3167c91af12205d37263
nicholas a. evans <nicholas.evans@gmail.com>
2021-11-05 01:49:20 -0400

    WIP: Create EM::SSL::Context, ETC, ETC, ETC
    NOTE: there _are_ merge/rebase mistakes in this commit.
    NOTE: 23 tests are failing in pure_ruby mode.
    
    * Create EM::SSL::Context
      * Rewrite EM::Connection#start_tls
        * use EM::SSL::Context with EM::SSL::Context#set_params
          * This changes the default options to more closely mimic stdlib!
        * Rewrite rdoc (yardoc)
      * use struct em_ssl_ctx to encapsulate all of the tls parms in C/C++
        * struct em_ssl_ctx
        * move most SslBox_t properties onto em_ssl_ctx struct
        * extract_ssl_context_struct
        * EM_SSL_CTX_COPY_IVAR macros
        * C++ try/catch, RB_GC_GUARD
      * Add #cert_store (stub, only allows default for now)
      * Add #to_stdlib_ssl_ctx for pure ruby version
      * Add ... more ...
    
    * SSLContext_t constructor: copy or extract methods:
      * em_ossl_sslctx_set_options
      * em_ossl_sslctx_set_minmax_proto_version
        * SSL_CTX_set_min_proto_version
        * SSL_CTX_set_max_proto_version
      * em_ossl_sslctx_set_cert_store
      * em_ossl_sslctx_set_ca_file_and_path
        * SSL_CTX_load_verify_file
        * SSL_CTX_load_verify_dir
      * em_ossl_sslctx_use_certificate
      * em_ossl_sslctx_set_default_certificate
      * em_ossl_sslctx_set_cipher_list
      * em_ossl_sslctx_set_tmp_dh
      * em_ossl_sslctx_set_tmp_ecdh
    
    * Set X509 Cert store on SSL_CTX
      * bDefaultX509Store
        * InitializeDefaultX509Store
      * em_ossl_sslctx_set_cert_store
        * SSL_CTX_set1_cert_store (or SSL_CTX_set_cert_store)
    
    * Updates to verify callback:
      * Only set verify callback if not VERIFY_NONE
        * SSL_CTX_set_verify
    
    * Extract utility function:
      * em_ossl_init
        * Uses SSL_get_ex_new_index
          * em_ssl_ssl_ex_binding_idx
          * em_ssl_ssl_ex_ptr_idx
      * em_ossl_throw_errors
    * Start work on hostname verification (Add bVerifyHostname)

--- lib/eventmachine.rb
+++ lib/eventmachine.rb
@@ -23,6 +23,7 @@ require 'em/iterator'
 require 'em/buftok'
 require 'em/timers'
 require 'em/protocols'
+require 'em/ssl'
 require 'em/connection'
 require 'em/ssl'
 require 'em/callback'
@@ -121,16 +122,16 @@ module EventMachine
   # @example Starting EventMachine event loop in the current thread to run the "Hello, world"-like Echo server example
   #
   #   #!/usr/bin/env ruby
-  #   
+  #
   #   require 'rubygems' # or use Bundler.setup
   #   require 'eventmachine'
-  #   
+  #
   #   class EchoServer < EM::Connection
   #     def receive_data(data)
   #       send_data(data)
   #     end
   #   end
-  #   
+  #
   #   EventMachine.run do
   #     EventMachine.start_server("0.0.0.0", 10000, EchoServer)
   #   end
@@ -382,24 +383,24 @@ module EventMachine
   #
   #   require 'rubygems'
   #   require 'eventmachine'
-  #   
+  #
   #   module Redmond
   #     def post_init
   #       puts "We're sending a dumb HTTP request to the remote peer."
   #       send_data "GET / HTTP/1.1\r\nHost: www.microsoft.com\r\n\r\n"
   #     end
-  #   
+  #
   #     def receive_data data
   #       puts "We received #{data.length} bytes from the remote peer."
   #       puts "We're going to stop the event loop now."
   #       EventMachine::stop_event_loop
   #     end
-  #   
+  #
   #     def unbind
   #       puts "A connection has terminated."
   #     end
   #   end
-  #   
+  #
   #   puts "We're starting the event loop now."
   #   EventMachine.run {
   #     EventMachine.connect "www.microsoft.com", 80, Redmond
